acaf56096df67f6eec01d60d3705c22ab4b53a0b
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-05-20 03:28:17 -0500

    Update WyScoutProcessData.py

--- WyScoutProcessData.py
+++ WyScoutProcessData.py
@@ -49,11 +49,11 @@ def colorlist(color1, color2, num):
 
 #####################################################################################################################################################
 
-font_path = 'C:/Users/ACER/Documents/WinStats/Resources/fonts/keymer-bold.otf'  # Your font path goes here
+font_path = 'keymer-bold.otf'  # Your font path goes here
 font_manager.fontManager.addfont(font_path)
 prop2 = font_manager.FontProperties(fname=font_path)
 
-font_path2 = 'C:/Users/ACER/Documents/WinStats/Resources/fonts/BasierCircle-Italic.ttf'  # Your font path goes here
+font_path2 = 'BasierCircle-Italic.ttf'  # Your font path goes here
 font_manager.fontManager.addfont(font_path2)
 prop3 = font_manager.FontProperties(fname=font_path2)
 
