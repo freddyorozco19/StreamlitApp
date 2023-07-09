88f963412014ff44589a8ed1a39ae2f249cd7692
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-07-09 18:31:57 -0500

    Update WyScoutProcessData.py

--- WyScoutProcessData.py
+++ WyScoutProcessData.py
@@ -632,6 +632,8 @@ if menu_id == "AllMetrics":
     ax.patch.set_facecolor('#050E1E')
     x = df[metselFK]
     y = df[metselFK2]
+    st.write(x)
+    st.write(y)
     ax.scatter(x, y , color='#FF0046')
     st.pyplot(fig, bbox_inches="tight", dpi=600, format="png")
     #st.markdown("<style> div { text-align: center; color: #FFFFFF } </style>", unsafe_allow_html=True)
