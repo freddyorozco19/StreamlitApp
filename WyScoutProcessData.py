0827cbafda9979df3880160934d5c77f96e4079b
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-07-11 12:04:00 -0500

    Update WyScoutProcessData.py

--- WyScoutProcessData.py
+++ WyScoutProcessData.py
@@ -921,8 +921,8 @@ if menu_id == "PlayerStats":
         with st.form(key='form3'):
             #SELECT DATA
             Dataframe = st.file_uploader("Cargar archivo:", type="xlsx")
-            if Dataframe is not None:
-                df = pd.read_excel(Dataframe)
+            #if Dataframe is not None:
+            df = pd.read_excel(Dataframe)
             #SELECT AGE
             agesel = st.slider('Filtro de edad:', 15, 50, (15, 50), 1)   
             df = df[df['Age'] <= agesel[1]]
