4d2801ea1e087ca5c37739cdb07b11ea980e008f
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-07-13 15:52:50 -0500

    Update WyScoutProcessData.py

--- WyScoutProcessData.py
+++ WyScoutProcessData.py
@@ -1197,8 +1197,8 @@ if menu_id == "PlayerStats":
 
     colorradar1 = "#FF0046"
     colorradar2 = "#005CBE"
-    alpharradar1 = 0.5
-    alpharradar2 = 0.9
+    alpharradar1 = 0.7
+    alpharradar2 = 0.7
     
     with space0:
         fig, ax = radarofe.setup_axis()  # format axis as a radar
