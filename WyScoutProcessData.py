a8f52167da2108f60efd9b8a24ba015b74f6a39f
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-07-13 16:17:33 -0500

    Update WyScoutProcessData.py

--- WyScoutProcessData.py
+++ WyScoutProcessData.py
@@ -1382,8 +1382,8 @@ if menu_id == "PlayerStats":
 
     #####
     fig, ax = plt.subplots(figsize = (12,1), dpi=600)
-    fig.set_facecolor('#121214')
-    ax.patch.set_facecolor('#121214')
+    #fig.set_facecolor('#121214')
+    ax.patch.set_visible('False')
     ax.set_xlim(0,100)
     ax.set_ylim(0,10)
     ax.scatter(30, 5, s=600, color=colorradar1, marker='s')
