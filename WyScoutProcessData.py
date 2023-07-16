75784dc5a58ecd59ad80ee438eeb0fbc53c81f36
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2023-07-16 14:24:30 -0500

    Update WyScoutProcessData.py

--- WyScoutProcessData.py
+++ WyScoutProcessData.py
@@ -1570,3 +1570,34 @@ if menu_id == "PlayerStats":
     )
     
     st.altair_chart(c)
+
+    data = pd.DataFrame({
+        'x': [1, 2, 3, 4, 5],
+        'y': [5, 4, 3, 2, 1],
+        'labels': ['A', 'B', 'C', 'D', 'E']
+    })
+    
+    # Crear el gráfico de dispersión usando Matplotlib
+    fig, ax = plt.subplots()
+    ax.scatter(data['x'], data['y'])
+    
+    # Función para mostrar el tooltip cuando el mouse esté sobre un punto
+    def hover(event):
+        # Obtener las coordenadas del punto donde está el mouse
+        x, y = event.xdata, event.ydata
+        
+        # Buscar el punto más cercano en los datos reales
+        distances = ((data['x'] - x) ** 2 + (data['y'] - y) ** 2) ** 0.5
+        nearest_point = distances.idxmin()
+        
+        # Obtener las etiquetas correspondientes al punto más cercano
+        label = data.loc[nearest_point, 'labels']
+        
+        # Mostrar el tooltip con la etiqueta
+        st.tooltip(label)
+    
+    # Conectar el evento de hover al gráfico de Matplotlib
+    cid = fig.canvas.mpl_connect('motion_notify_event', hover)
+    
+    # Mostrar el gráfico utilizando Streamlit
+    st.pyplot(fig)
